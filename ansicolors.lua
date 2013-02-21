a209800d08a2b6a2445d2033cece0ed29fee1a2e
kikito <kikito@gmail.com>
2013-02-21 23:46:08 +0100

    bump to 1.0.2

--- ansicolors.lua
+++ ansicolors.lua
@@ -1,4 +1,4 @@
--- ansicolors.lua v1.0.1 (2012-08)
+-- ansicolors.lua v1.0.2 (2012-08)
 
 -- Copyright (c) 2009 Rob Hoelz <rob@hoelzro.net>
 -- Copyright (c) 2011 Enrique García Cota <enrique.garcia.cota@gmail.com>
